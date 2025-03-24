1b5361ba0814211b47c153c02cc656dd6a3f2d17
Roy, Nicolas <roy.nicolas.10@courrier.uqam.ca>
2025-03-24 11:48:00 -0400

    Added fonctions to manage directions to refactor editorMoveCursor in kilo_nico.c

--- kilo_nico.c
+++ kilo_nico.c
@@ -944,40 +944,50 @@ char *editorPrompt(char *prompt , void (*callback)(char *, int)) {
   }
 }
 
+void cursorMoveLeft() {
+  if (E.cx != 0) {
+    E.cx--;
+  } else if (E.cy > 0) {
+    E.cy--;
+    E.cx = E.row[E.cy].size;
+  }
+}
+
+void cursorMoveRight() {
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  if (row && E.cx < row->size) {
+    E.cx++;
+  } else if (row && E.cx == row->size) {
+    E.cy++;
+    E.cx = 0;
+  }
+}
+
+void cursorMoveUp() {
+  if (E.cy != 0) {
+    E.cy--;
+  }
+}
+
+void cursorMoveDown() {
+  if (E.cy < E.numrows) {
+    E.cy++;
+  }
+}
+
 void editorMoveCursor(const int key) {
   erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
-  switch (key) {
-    case ARROW_LEFT:
-      if (E.cx != 0) {
-        E.cx--;
-      } else if (E.cy > 0) {
-        E.cy--;
-        E.cx = E.row[E.cy].size;
-      }
-      break;
-    case ARROW_RIGHT:
-      if (row && E.cx < row->size) {
-        E.cx++;
-      } else if (row && E.cx == row->size) {
-        E.cy++;
-        E.cx = 0;
-      }
-      break;
-    case ARROW_UP:
-      if (E.cy != 0) {
-        E.cy--;
-      }
-      break;
-    case ARROW_DOWN:
-      if (E.cy < E.numrows) {
-        E.cy++;
-      }
-      break;
+  if (key == ARROW_LEFT) {
+    cursorMoveLeft();
+  } else if (key == ARROW_RIGHT) {
+    cursorMoveRight();
+  } else if (key == ARROW_UP) {
+    cursorMoveUp();
+  } else if (key == ARROW_DOWN) {
+    cursorMoveDown();
   }
   int rowlen = row ? row->size : 0;
-  if (E.cx > rowlen) {
-    E.cx = rowlen;
-  }
+  E.cx = E.cx > rowlen ? rowlen : E.cx;
 }
 
 void editorMoveCursorPage(int key) {
